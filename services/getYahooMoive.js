847858581d82da2982c1369b9a48a5f73aceb181
zxcvbn96519 <zxcvbn96519@gmail.com>
2018-08-17 21:56:11 +0800

    🎨 Style: add try catch to fetch data

--- services/getYahooMoive.js
+++ services/getYahooMoive.js
@@ -34,61 +34,69 @@ exports.question = async function (q) {
 }
 
 exports.firstSearch = async function (url) {
-  const body = await getBody(url)
-  const $ = cheerio.load(body)
-  let datas = []
-  $('.searchpage ul li').each((i, el) => {
-    const go = cheerio.load($(el).html())
-    let tmp = go('.release_movie_name')
-      .text()
-      .split('\n')
-    if (tmp[1] !== undefined) {
-      i++
-      let key = tmp[1].trim()
-      let value = go('.release_movie_name a').attr('href')
-      datas[i] = {
-        name: key,
-        url: value,
-        searchable: true
+  try {
+    const body = await getBody(url)
+    const $ = cheerio.load(body)
+    let datas = []
+    $('.searchpage ul li').each((i, el) => {
+      const go = cheerio.load($(el).html())
+      let tmp = go('.release_movie_name')
+        .text()
+        .split('\n')
+      if (tmp[1] !== undefined) {
+        i++
+        let key = tmp[1].trim()
+        let value = go('.release_movie_name a').attr('href')
+        datas[i] = {
+          name: key,
+          url: value,
+          searchable: true
+        }
+        console.log(i + ' : ' + key)
       }
-      console.log(i + ' : ' + key)
-    }
-  })
-  return datas
+    })
+    return datas
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 exports.secondSearch = async function (url) {
-  const body = await getBody(url)
-  const $ = cheerio.load(body)
-  let datas = {}
-  datas['src'] = $(
-    '#maincontainer #content_l .movie_intro_info .movie_intro_info_l .movie_intro_foto img'
-  ).attr('src')
-  $('#maincontainer #content_l .movie_intro_info .movie_intro_info_r').each(
-    (i, el) => {
-      const go = cheerio.load($(el).html())
-      datas['title'] = go('h1').text() + ' ' + go('h3').text()
-      // datas['title_en'] = go("h3").text()
-      go('span').each((i, el) => [
-        (datas['info_' + i] = $(el)
-          .text()
-          .split('：')[1])
-      ])
-      go('.movie_intro_list').each((i, el) => {
-        datas['info_' + (i + 3)] = $(el)
-          .text()
-          .split('\n')
-        let tmp = ''
-        datas['info_' + (i + 3)].forEach(element => {
-          if (element.trim() !== '' && element.trim() !== '、') {
-            tmp += element.trim() + ', '
-          }
+  try {
+    const body = await getBody(url)
+    const $ = cheerio.load(body)
+    let datas = {}
+    datas['src'] = $(
+      '#maincontainer #content_l .movie_intro_info .movie_intro_info_l .movie_intro_foto img'
+    ).attr('src')
+    $('#maincontainer #content_l .movie_intro_info .movie_intro_info_r').each(
+      (i, el) => {
+        const go = cheerio.load($(el).html())
+        datas['title'] = go('h1').text() + ' ' + go('h3').text()
+        // datas['title_en'] = go("h3").text()
+        go('span').each((i, el) => [
+          (datas['info_' + i] = $(el)
+            .text()
+            .split('：')[1])
+        ])
+        go('.movie_intro_list').each((i, el) => {
+          datas['info_' + (i + 3)] = $(el)
+            .text()
+            .split('\n')
+          let tmp = ''
+          datas['info_' + (i + 3)].forEach(element => {
+            if (element.trim() !== '' && element.trim() !== '、') {
+              tmp += element.trim() + ', '
+            }
+          })
+          datas['info_' + (i + 3)] = tmp.substring(0, tmp.length - 2).replace(/"\\"/g, '')
         })
-        datas['info_' + (i + 3)] = tmp.substring(0, tmp.length - 2).replace(/"\\"/g, '')
-      })
-    }
-  )
-  datas['content'] = $('#maincontainer #content_l .l_box .gray_infobox_inner').text().trim().replace('\n          \n          詳全文', '').replace(/\n/g, '<br>')
-  datas['content'] !== '' ? console.log('Fetch ok') : console.log('Fetch fail')
-  return datas
+      }
+    )
+    datas['content'] = $('#maincontainer #content_l .l_box .gray_infobox_inner').text().trim().replace('\n          \n          詳全文', '').replace(/\n/g, '<br>')
+    datas['content'] !== '' ? console.log('Fetch ok') : console.log('Fetch fail')
+    return datas
+  } catch (e) {
+    console.log(e)
+  }
 }
